7557a7e8d50307ac812f16edb6ee5af5e6a91938
jmoseka <jamylam3@gmail.com>
2022-09-14 22:18:54 +0300

    Automatic fix rubocop

--- app/controllers/api/v1/greetings_controller.rb
+++ app/controllers/api/v1/greetings_controller.rb
@@ -1,6 +1,12 @@
-class Api::V1::GreetingsController < ApplicationController
-  def index
-    @greetings = Greeting.all.sample.greeting
-    render json: { greeting: @greetings }
+# frozen_string_literal: true
+
+module Api
+  module V1
+    class GreetingsController < ApplicationController
+      def index
+        @greetings = Greeting.all.sample.greeting
+        render json: { greeting: @greetings }
+      end
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
@@ -7,7 +9,6 @@ Rails.application.routes.draw do
   # get 'root/index'
   root 'root#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  
 
   # Defines the root path route ("/")
   # root "articles#index"
